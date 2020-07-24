c52c1ae2e220099636b94d4935ba8244206a3bbb
Juan <juanpablo.pr1997@gmail.com>
2020-07-24 20:42:20 -0300

    Ejercicio 5.4

---
+++ Automovil.java
@@ -0,0 +1,10 @@
+package practicas;
+
+abstract class Automovil {
+
+	abstract void retroceder();
+	
+	public void avanzar() {
+		System.out.println("Soy el metodo avanzar de Automovil");
+	}
+}
---
+++ Berlina.java
@@ -0,0 +1,16 @@
+package practicas;
+
+public class Berlina extends Automovil{
+
+	@Override
+	void retroceder() {
+		
+	}
+	public static void main(String[] args) {
+		Berlina berlina = new Berlina();
+		berlina.avanzar();
+	}
+	
+	
+
+}
